16ed440111b05af02534796966e501843d413f8c
HimaashreeBorah <62025303+HimaashreeBorah@users.noreply.github.com>
2020-03-31 16:21:56 +0530

    Modified need matrix

--- resource.cpp
+++ resource.cpp
@@ -39,7 +39,45 @@ int main()
       }
       
       //available matrix
+      cout<<"\nEnter the Available Resources:\n";
+      for(i= 0; i <n; i++) { 
+            cin>>available[i];
+      }
       
+      int f[n], ans[n], ind = 0; 
+      for (k = 0; k <n; k++) { 
+        f[k] = 0; 
+    }
+	//need matrix
+	int need[n][m]; 
+    for (i = 0; i < n; i++) { 
+        for (j = 0; j < m; j++) 
+            need[i][j] = max[i][j] - allocated[i][j]; 
+		}
+		
+		
+	 int y = 0; 
+    for (k = 0; k < 5; k++) { 
+        for (i = 0; i < n; i++) { 
+            if (f[i] == 0) { 
+  
+                int flag = 0; 
+                for (j = 0; j < m; j++) { 
+                    if (need[i][j] > available[j]){ 
+                        flag = 1; 
+                        break; 
+                    } 
+                } 
+  
+                if (flag == 0) { 
+                    ans[ind++] = i; 
+                    for (y = 0; y < m; y++) 
+                        available[y] += allocated[i][y]; 
+                    f[i] = 1; 
+                } 
+            } 
+        } 
+    } 
 	
 	cout<< "Following is the SAFE Sequence"<<endl;
     for (i = 0; i < n - 1; i++) 
